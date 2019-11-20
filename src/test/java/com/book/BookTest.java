cae10f05a7f4b1af5580a1735fdfedc5b1499108
Alex K <alexey.kondrashov1@uk.ey.com>
2019-11-20 13:17:30 +0000

    Add first test

---
+++ src/test/java/com/book/BookTest.java
@@ -0,0 +1,14 @@
+package com.book;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BookTest {
+
+	@Test
+	public void testBook() {
+		assertEquals(7, 7);
+	}
+
+}
